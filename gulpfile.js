f1e95b298075f1e4caeaffab796e176a83f5ca41
Artem Verbat <artem.verbat@gmail.com>
2017-05-27 14:30:44 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,161 @@
+const gulp = require('gulp');
+const buffer = require('vinyl-buffer');
+const postcss = require('gulp-postcss');
+const stylus = require('gulp-stylus');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
+const gulpIf = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const del = require('del');
+const gutil = require('gulp-util');
+const pug = require('gulp-pug');
+const spritesmith = require('gulp.spritesmith');
+const tinypng = require('gulp-tinypng-nokey');
+const webpack = require('webpack');
+const changed = require('gulp-changed');
+const svgSymbols = require('gulp-svg-symbols');
+const svgmin = require('gulp-svgmin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+
+gulp.task('views', function buildHTML() {
+  return gulp.src('./src/views/pages/*.pug')
+    .pipe(pug())
+    .on('error', function(error) {
+      gutil.log(gutil.colors.red('Error: ' + error.message));
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('./src/styles/app.styl')
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(stylus({
+      'include css': true
+    })
+    .on('error', function(error) {
+      gutil.log(gutil.colors.red('Error: ' + error.message));
+      this.emit('end');
+    }))
+    .pipe(gulpIf(!isDevelopment, postcss([
+      autoprefixer({
+          browsers: ['> 5%', 'ff > 14']
+      })
+    ])))
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+    .pipe(gulpIf(!isDevelopment, cleanCSS()))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('scripts', function(done) {
+  return webpack(require('./webpack.config.js'), function(error, stats) {
+    if (error) throw new gutil.PluginError('webpack', error);
+
+    gutil.log('[scripts]', stats.toString({
+      colors: gutil.colors.supportsColor,
+      chunks: false,
+      hash: false,
+      version: false
+    }));
+
+    done();
+  });
+});
+
+gulp.task('fonts', function () {
+  return gulp.src([
+    './src/fonts/**/*.*'
+  ])
+    .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('sprite', function() {
+  const spriteData = gulp.src('./src/images/sprite/*.{png,jpg,jpeg}')
+    .pipe(spritesmith({
+      imgName: 'sprite.png',
+      cssName: 'images.styl',
+      algorithm: 'binary-tree',
+      padding: 2,
+      cssTemplate: './src/styles/sprites/sprite-template.mustache'
+    }));
+
+  spriteData.img
+    .pipe(buffer())
+    .pipe(gulpIf(!isDevelopment, tinypng()))
+    .pipe(gulp.dest('./public/images'));
+
+  spriteData.css.pipe(gulp.dest('./src/styles/sprites'));
+
+  return spriteData;
+});
+
+gulp.task('svgSymbols', function () {
+  return gulp.src('./src/images/svg/**/*.svg')
+    .pipe(svgmin())
+    .pipe(svgSymbols({
+      templates: ['default-svg'],
+      className: '.icon_%f'
+    }))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('images', function () {
+  return gulp.src(['./src/images/**/*.*', '!./src/images/sprite'])
+    .pipe(changed('./public/images'))
+    .pipe(gulpIf(!isDevelopment, gulpIf(['!*.svg'], tinypng())))
+    .pipe(gulp.dest('./public/images'));
+});
+
+gulp.task('misc', function () {
+  return gulp.src('./src/misc/**/*.*')
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/views/**/*.pug', gulp.series('views'));
+  gulp.watch('./src/styles/**/*.{css,styl}', gulp.series('styles'));
+  gulp.watch('./src/scripts/**/*.js', gulp.series('scripts'));
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    // proxy: 'example.com',
+    // files: 'public/**/*.*',
+    // https: true,
+    server: './public',
+    port: 8080
+  });
+
+  browserSync.watch('./public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('clean', function () {
+  return del('./public')
+});
+
+gulp.task('build', gulp.series(
+  'clean',
+  gulp.parallel(
+    'sprite',
+    'svgSymbols'
+  ),
+  gulp.parallel(
+    'views',
+    'styles',
+    'scripts',
+    'fonts',
+    'images',
+    'misc'
+  )));
+
+gulp.task('default', gulp.series(
+  'build',
+  gulp.parallel(
+    'watch',
+    'serve'
+  )));
