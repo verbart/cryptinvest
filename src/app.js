b9a79cb8f2e8eaab618686a9b8a56a8a3b05fdba
Artem Verbat <artem.verbat@gmail.com>
2017-05-29 11:16:11 +0300

    Added testimonials

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
-import './components/main-header/main-header'
-import './components/navbar/navbar'
+import 'jquery';
+import 'slick-carousel';
+
+import './components/main-header/main-header';
+import './components/navbar/navbar';
+import './components/sections/testimonials/testimonials';
