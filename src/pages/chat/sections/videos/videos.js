e6c7771a41c7fff5aff9cb67448e76f5cfe9d569
Artem Verbat <artem.verbat@gmail.com>
2017-06-25 19:29:37 +0300

    Updated prise section

--- src/pages/chat/sections/videos/videos.js
+++ src/pages/chat/sections/videos/videos.js
@@ -20,22 +20,18 @@ $('.videoPreview').click(function (e) {
 function toggleVideo(e, play) {
   const iframe = $(e.target).find('iframe')[0].contentWindow;
   const func = play ? 'playVideo' : 'stopVideo';
-  console.log(func);
 
   iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
 }
 
 $(document).on('opened', '.remodal', function(e) {
   if ($(e.target).hasClass('videoModal')) {
-    console.log(1111111111111);
     setTimeout(() => toggleVideo(e, true), 500);
   }
 });
 
 $(document).on('closing', '.remodal', function(e) {
   if ($(e.target).hasClass('videoModal')) {
-    console.log(22222222222222);
-
     toggleVideo(e);
   }
 });
